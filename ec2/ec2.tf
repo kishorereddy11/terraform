26ce44302841bbf68eda0450aa41cfc9db4c971a
kishor <kishorebsc12@gmail.com>
2025-02-01 11:15:24 +0530

    example

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,12 +1,8 @@
 resource "aws_instance" "myserver" {
-  ami = "ami-09c813fb71547fc4f"
+  ami = var.ami_id
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "terraform_Demo"
-    purpose = "practice"
-  }
+  instance_type = var.instance_type
+  tags = var.ec2_tags
 
   
 }
@@ -23,13 +19,12 @@ resource "aws_security_group" "allow_tls" {
     }
 
     ingress  {
-        from_port = 22
-        to_port = 22
+        from_port = var.from_port
+        to_port = var.to_port
         protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
+        cidr_blocks = var.cidr_blocks
     }
 
-    tags = {
-      name = "allow_tls"
-    }  
+    tags =  var.sg_tags
+    
 }
